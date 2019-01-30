3cce1b3f1b5810494925b925f40b1cd5446d0bcf
zhaoyao <15138067756@163.com>
2019-01-30 10:29:51 +0800

    add time-demo

---
+++ JDK/时间处理/time-demo/src/main/java/com/example/timedemo/TimeDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.timedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeDemoApplication.class, args);
+    }
+
+}
+
