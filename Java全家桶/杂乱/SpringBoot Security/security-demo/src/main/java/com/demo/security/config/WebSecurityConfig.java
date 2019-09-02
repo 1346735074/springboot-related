3a9caf4d480316fb2cf44cd6a3b5ef0607656d0e
Purgeyao <zhaoy@deepblueai.com>
2019-09-02 16:32:47 +0800

    test

--- Java全家桶/杂乱/SpringBoot Security/security-demo/src/main/java/com/demo/security/config/WebSecurityConfig.java
+++ Java全家桶/杂乱/SpringBoot Security/security-demo/src/main/java/com/demo/security/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.demo.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
